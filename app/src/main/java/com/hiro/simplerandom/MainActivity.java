6a983829310551e4e15e8a7854be4157c6780486
Văn Phát Lê <vanphat13it@gmail.com>
2020-03-21 13:55:04 +0700

    Update random function : generate number that doesn't exists in history.

--- app/src/main/java/com/hiro/simplerandom/MainActivity.java
+++ app/src/main/java/com/hiro/simplerandom/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     TextView txvResult;
     EditText etxMin, etxMax ;
     Button btnGenerate;
+
+    ArrayList<Integer> randomlist;
+    boolean isExists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         etxMin = (EditText) findViewById(R.id.etxMin);
         etxMax = (EditText) findViewById(R.id.etxMax);
         btnGenerate = (Button) findViewById(R.id.btnGenerate);
+        randomlist = new ArrayList<>();
 
         btnGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +40,21 @@ public class MainActivity extends AppCompatActivity {
                     int min = Integer.parseInt(etxMin.getText().toString());
                     int max = Integer.parseInt(etxMax.getText().toString());
                     int result = random.nextInt(max - min + 1) + min;
+                    if(randomlist.size()>(max - min)){
+                        Toast.makeText(MainActivity.this, "Đã hết số để random, ứng dụng sẽ random lại từ đầu!", Toast.LENGTH_LONG).show();
+                        randomlist.clear();
+                    }
+
+                    if (randomlist.size() == 0 || randomlist == null) {
+                        randomlist.add(result);
+                    } else {
+                        while (IsExist(result, randomlist)){
+                            result = random.nextInt(max - min + 1) + min;
+                        }
+                        randomlist.add(result);
+                    }
                     txvResult.setText(Integer.toString(result));
+
                 } catch (Exception e){
                     Toast.makeText(MainActivity.this, "Lỗi, xin nhập số đầy đủ và chính xác! " +
                             "Nhập số bắt đầu vào min và số cuối của dãy cần random vào max", Toast.LENGTH_LONG).show();
@@ -43,5 +62,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public boolean IsExist(int num, ArrayList<Integer> randomlist){
+        boolean result = false;
+        for (int i : randomlist) {
+            if (num == i){
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
